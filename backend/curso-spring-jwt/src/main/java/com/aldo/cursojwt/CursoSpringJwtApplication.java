aa4a0719da9be1e05fc3371de2c850a3db8c8cea
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-06-12 23:11:11 -0300

    Projeto Criado

---
+++ backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/CursoSpringJwtApplication.java
@@ -0,0 +1,13 @@
+package com.aldo.cursojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringJwtApplication.class, args);
+	}
+
+}
---
+++ backend/curso-spring-jwt/src/test/java/com/aldo/cursojwt/CursoSpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldo.cursojwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
