cd157b929fda3501c79473afe8cb22d2f7caddb0
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-06-14 16:12:20 -0300

    Criando a operação de instanciação:CommandLineRunner

--- backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/CursoSpringJwtApplication.java
+++ backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/CursoSpringJwtApplication.java
@@ -1,13 +1,33 @@
 package com.aldo.cursojwt;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoSpringJwtApplication {
+import com.aldo.cursojwt.domain.Categoria;
+import com.aldo.cursojwt.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoSpringJwtApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringJwtApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "INFORMÀTICA");
+		Categoria cat2 = new Categoria(null, "ESCRITÓRIO");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+
+	
 }
