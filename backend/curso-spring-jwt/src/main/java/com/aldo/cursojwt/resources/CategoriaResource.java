e1948cae469fed88907a7ab70910bb2740d8ceaa
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-06-20 22:03:23 -0300

    Iserindo uma nova Categoria com POST

--- backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/resources/CategoriaResource.java
+++ backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.aldo.cursojwt.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aldo.cursojwt.domain.Categoria;
 import com.aldo.cursojwt.services.CategoriaService;
@@ -25,6 +29,13 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	
+	@RequestMapping( method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria){
+		categoria = service.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 }
