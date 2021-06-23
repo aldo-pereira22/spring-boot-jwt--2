1321039e0587b403b7a7940e4d3bbb36284facd9
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-06-23 17:08:28 -0300

    Validação customizada: Email não repetido na inserção de cliente

--- backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/repositories/ClienteRepository.java
+++ backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.aldo.cursojwt.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,7 @@ import com.aldo.cursojwt.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
 	
+	@Transactional
+	Cliente findByEmail(String email);
 }
--- backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/services/validation/ClienteInsertValidator.java
+++ backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.aldo.cursojwt.domain.Cliente;
 import com.aldo.cursojwt.domain.enuns.TipoCliente;
 import com.aldo.cursojwt.dto.ClienteNewDTO;
+import com.aldo.cursojwt.repositories.ClienteRepository;
 import com.aldo.cursojwt.resources.exceptions.FieldMessage;
 import com.aldo.cursojwt.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO > {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ INVÀLIDO"));
 		}
 		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if(aux != null ) {
+			list.add(new FieldMessage("email", "Email ja existente"));
+		}
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
