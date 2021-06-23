6f5d5fc0804c759c6281f1bd724097e64463da4e
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-06-23 16:57:59 -0300

    Validação personalizada de CPF ou CNPJ

---
+++ backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package com.aldo.cursojwt.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.aldo.cursojwt.domain.enuns.TipoCliente;
+import com.aldo.cursojwt.dto.ClienteNewDTO;
+import com.aldo.cursojwt.resources.exceptions.FieldMessage;
+import com.aldo.cursojwt.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO > {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CPF INVALIDO"));
+		}
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CNPJ INVÀLIDO"));
+		}
+		
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
