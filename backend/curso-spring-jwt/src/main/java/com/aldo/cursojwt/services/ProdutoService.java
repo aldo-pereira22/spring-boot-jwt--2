5456418321c8ce065c60ff71b9e440e99bb0382f
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-06-24 17:29:55 -0300

    Busca de produtos por nome e categorias

---
+++ backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/services/ProdutoService.java
@@ -0,0 +1,41 @@
+package com.aldo.cursojwt.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.aldo.cursojwt.domain.Categoria;
+import com.aldo.cursojwt.domain.Produto;
+import com.aldo.cursojwt.repositories.CategoriaRepository;
+import com.aldo.cursojwt.repositories.ProdutoRepository;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+
+	public Produto find(Integer id) throws ObjectNotFoundException {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids,Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+
+}
