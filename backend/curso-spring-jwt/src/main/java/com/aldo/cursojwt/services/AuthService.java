4042f93440b614cd59825949cf1e8bffcccfd3a4
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-01 18:43:25 -0300

    Nova senha

---
+++ backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/services/AuthService.java
@@ -0,0 +1,74 @@
+package com.aldo.cursojwt.services;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.aldo.cursojwt.domain.Cliente;
+import com.aldo.cursojwt.repositories.ClienteRepository;
+import com.aldo.cursojwt.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class AuthService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	
+	@Autowired
+	private EmailService emailService;
+	
+	private Random rand = new Random();
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
+	
+	
+	public void sendNewPassword(String email) {
+		
+		Cliente cliente = clienteRepository.findByEmail(email);
+		
+		if(cliente == null) {
+			throw new ObjectNotFoundException("Email não encontrado!");
+		}
+		String newPass = newPassword();
+		cliente.setSenha(pe.encode(newPass));
+		clienteRepository.save(cliente);
+		emailService.sendNewPasswordEmail(cliente, newPass);
+	}
+
+
+	private String newPassword() {
+		char[] vet = new char[10];
+		for(int i = 0; i < 10; i++) {
+			vet[i] = randomChar();
+		}
+		
+		return new String(vet);
+	}
+
+
+	private char randomChar() {
+		int opt = rand.nextInt(3);
+		if(opt == 0) { // gera um dígito
+			
+			return (char)(rand.nextInt(10) + 48);
+			
+		}else if(opt == 1) { //Gera letra maiscula
+			
+			return (char)(rand.nextInt(26) + 65);
+			
+		}else { //Gera letra minuscula
+			return (char)(rand.nextInt(10) + 97);
+			
+		}
+	}
+}
+
+
+
+
+
+
