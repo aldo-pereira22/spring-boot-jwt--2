7edd0cf66cf419f7d4f23d05a6be56e91af20293
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-07-01 15:30:25 -0300

    Implementando autorização

--- backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/config/SecurityConfig.java
+++ backend/curso-spring-jwt/src/main/java/com/aldo/cursojwt/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.aldo.cursojwt.security.JWTAuthenticationFilter;
+import com.aldo.cursojwt.security.JWTAuthorizationFIlter;
 import com.aldo.cursojwt.security.JWTUtil;
 
 
@@ -41,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFIlter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
